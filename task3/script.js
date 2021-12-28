a7ecb552aa509a4e9093d4a19bd69abc0a8c2e81
Egor <Gambit47@yandex.ru>
2021-12-28 07:39:19 +0300

    update task3

--- task3/script.js
+++ task3/script.js
@@ -1,21 +1,20 @@
-const trafficLightEl = document.querySelector('#trafficLight');
+const parent = document.querySelector('.parent');
+const child = document.querySelectorAll("#trafficLight");
 
-function makeGreen() {
-    trafficLightEl.style.background = ('green');
-    trafficLightEl.removeEventListener('click', makeGreen);
-    trafficLightEl.addEventListener('click', makeYellow);
-}
-
-function makeYellow() {
-    trafficLightEl.style.background = ('yellow');
-    trafficLightEl.removeEventListener('click', makeYellow);
-    trafficLightEl.addEventListener('click', makeRed);
-}
-
-function makeRed() {
-    trafficLightEl.style.background = ('red');
-    trafficLightEl.removeEventListener('click', makeRed);
-    trafficLightEl.addEventListener('click', makeGreen);
-}
-
-trafficLightEl.addEventListener('click', makeGreen);
+parent.onclick = function (event) {
+    if (event.target == parent ) {
+    return;
+    } else if (event.target == child[0]) {
+        event.target.style.backgroundColor = 'green'
+        child[1].style.backgroundColor = 'black'
+        child[2].style.backgroundColor = 'black' 
+    } else if (event.target == child[1]) {
+        event.target.style.backgroundColor = 'yellow'
+        child[0].style.backgroundColor = 'black'
+        child[2].style.backgroundColor = 'black' 
+    } else if (event.target == child[2]) {
+        event.target.style.backgroundColor = 'red'
+        child[0].style.backgroundColor = 'black'
+        child[1].style.backgroundColor = 'black' 
+    };
+};
